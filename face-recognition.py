d2ef0aa34381e5086bdcf7c33737a619086deecd
inamcodes <maharinamullah50@gmail.com>
2026-06-11 18:31:14 +0500

    update face recognition

--- face-recognition.py
+++ face-recognition.py
@@ -3,33 +3,28 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
-cap.set(10, 100)  
+cap.set(10, 10)  
 
-if not cap.isOpened():
-    print("Error: Could not open webcam.")
-    exit()
 
 while True:
     
-    ret, frame = cap.read()
+    acceptence, frame = cap.read()
 
-    if not ret:
-        print("Error: Failed to grab frame.")
-        break
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces
     faces = face_cascade.detectMultiScale(
-        gray,
+        grayFrame,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30)
     )
 
-    # Draw rectangles around faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+10), (0, 255, 0), -1)
+        cv2.putText(frame, "MOOD", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
 
     cv2.imshow('Faces', frame)
 
